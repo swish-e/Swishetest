88f48aec37d6a7768db58ee0f18b952cc136a333
joshr <joshr@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2007-11-30 20:42:55 +0000

    bumped version to 0.05.

--- Swishetest.pm
+++ Swishetest.pm
@@ -11,7 +11,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw( build_index do_search) ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw( );
 
-our $VERSION = '0.04a';
+our $VERSION = '0.05';
 
 use DoSearch;
 use BuildIndex;
