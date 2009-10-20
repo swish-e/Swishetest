acd2f2a6937ba7431dc622d6f73d0117420890e1
joshr <joshr@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2009-10-20 22:07:12 +0000

    factor out common code between creating an index with an external program and from a directory; also record the config used to create the index with at $index.conf

--- BuildIndex.pm
+++ BuildIndex.pm
@@ -2,6 +2,9 @@
 package BuildIndex;
 use strict;
 use warnings; 
+use File::Copy;
+
+use Fatal qw(copy); # die if the copy of the config to $index.conf fails
 
 # given an Input directory, an Index to build, a Config file to use, and optional Extra Options,
 # returns a hash of info parsed from the indexing 
@@ -9,15 +12,11 @@ sub build_index_from_directory {
     my ($input, $index, $config, $extra_options) = @_; 
     $config = "conf/basic-libxml2.conf" unless $config;
     $extra_options = "" unless $extra_options; 
-	
-	# we assume swish-e is in the PATH!
     my $cmd = "swish-e -c $config -i '$input' -f '$index' -v 1 $extra_options";
-    my $output = `$cmd`;
-    print STDERR "$0: Running '$cmd'\n" if $ENV{TEST_VERBOSE};
-        # -v 1 is important, we use it to test the indexer 
-    die "$0: Didn't get any output from $cmd\n" unless $output;
-    return parse_indexing_output( $output );
-}
+
+    return build_index_with_cmd( $input, $index, $config, $cmd );
+}  
+
 
 # given an external prog, an Index to build, a Config file to use, and optional Extra Options,
 # returns a hash of info parsed from the indexing 
@@ -25,14 +24,25 @@ sub build_index_from_external_program {
     my ($external_program, $index, $config, $extra_options) = @_; 
     $config = "conf/basic-libxml2.conf" unless $config;
     $extra_options = "" unless $extra_options; 
-    # WE ASSUME SWISH-E is in the PATH
     my $cmd = "$external_program | swish-e -c $config -i stdin -f '$index' -v 1 -S prog $extra_options";
-        # -v 1 is important, we use it to test the indexer 
+
+    return build_index_with_cmd( $external_program, $index, $config, $cmd );
+}
+
+sub build_index_with_cmd {
+    my ($input, $index, $config, $cmd) = @_; 
+
+    # record the config file used
+    copy( $config, "$index.conf" );
+	
+	# we assume swish-e is in the PATH!
+    my $output = `$cmd`;
     print STDERR "$0: Running '$cmd'\n" if $ENV{TEST_VERBOSE};
-    my $output = `$cmd`;    
+        # -v 1 is important, we use it to test the indexer 
     die "$0: Didn't get any output from $cmd\n" unless $output;
     return parse_indexing_output( $output );
 }
+
 
 # given the output from an swish-e indexing run with '-v 1' (or greater) enabled,
 # returns a  hash of name->value pairs gleaned from the swish-e output
